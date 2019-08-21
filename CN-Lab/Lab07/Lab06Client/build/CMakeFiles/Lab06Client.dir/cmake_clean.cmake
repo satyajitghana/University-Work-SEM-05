d02d7b34b3c2aad8bf7d1aed2d9bb75330699df0
Satyajit Ghana <satyajitghana7@gmail.com>
2019-08-21 23:51:13 +0530

    Add CN-Lab06

---
+++ CN-Lab/Lab07/Lab06Client/build/CMakeFiles/Lab06Client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab06Client.dir/main.cpp.o"
+  "Lab06Client"
+  "Lab06Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab06Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CN-Lab/Lab07/Lab06Server/build/CMakeFiles/Lab06Server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab06Server.dir/main.cpp.o"
+  "Lab06Server"
+  "Lab06Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab06Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
